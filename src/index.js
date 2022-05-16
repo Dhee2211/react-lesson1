ed9249e66c2c1e358211bbda11d7bc1fff326d70
Dheerendra Panwar <Dhee2211@users.noreply.github.com>
2022-05-16 14:48:14 -0700

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+ReactDOM.render(
+  <div>
+    <h1>Where to show</h1>
+    <p>The world is mine</p>
+  </div>,
+  document.getElementById("root")
+);
